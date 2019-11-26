d3ae255696f5d684576c84aedb71c0e2e02f9117
kanadeiar <kanadeiar@gmail.com>
2019-11-26 22:00:46 +0300

    final task 1

--- HomeWorkLesson1/ConsoleApp1Form/Program.cs
+++ HomeWorkLesson1/ConsoleApp1Form/Program.cs
@@ -20,19 +20,57 @@ namespace ConsoleApp1Form
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            MyHeader(text: "Задача 1. Написать программу «Анкета». Последовательно задаются вопросы...");
-
-
-
+            MyHeader(text: "Задача 1. Написать программу «Анкета».");
+            ///////////////////////////////////////////////////////////////
+            string surname = getStringFromConsole("Введите фамилию человека");
+            string name = getStringFromConsole("Введите имя человека");
+            int age = getIntFromConsole("Введите возраст человека");
+            int growth = getIntFromConsole("Введите рост человека (см)");
+            int weight = getIntFromConsole("Введите вес человека (кг)");
+            WriteLine("\n\nВывод информации:\n");
+            WriteLine("Используя склеивание:");
+            WriteLine("Фамилия: " + surname + " Имя: " + name + " Возраст: " + age + " лет Рост: " + growth + " см Вес: " + weight + " кг.");
+            WriteLine("\nИспользуя форматированный вывод:");
+            WriteLine("Фамилия: {0} Имя: {1} Возраст: {2} лет Рост: {3} см Вес: {4} кг.", surname, name, age, growth, weight);
+            WriteLine("\nИспользуя вывод со знаком $:");
+            WriteLine($"Фамилия: {surname} Имя: {name} Возраст: {age} лет Рост: {growth} см Вес: {weight} кг.");
+            ///////////////////////////////////////////////////////////////
             MyFooter();
         }
-
+        /// <summary>
+        /// Получение строки с консоли
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static string getStringFromConsole(string message)
+        {
+            Write($"{message}:>");
+            return ReadLine();
+        }
+        /// <summary>
+        /// Получение числа с консоли
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static int getIntFromConsole(string message)
+        {
+            while (true)
+            {
+                Write($"{message}:>");
+                if (int.TryParse(ReadLine(), out int number))
+                {
+                    return number;
+                }
+                WriteLine("Ошибка! Введен неверный формат целого числа!");
+                Beep(500,500);
+            }
+        }
         /// <summary>
         /// Вывод моей шапки консольного приложения
         /// </summary>
         /// <param name="title"></param>
         /// <param name="text"></param>
-        static void MyHeader(string title = "Geekbrains. C# Уровень 1.", string text = "")
+        private static void MyHeader(string title = "Geekbrains. C# Уровень 1.", string text = "")
         {
             Title = title;
             WindowWidth = 120;
@@ -45,10 +83,13 @@ namespace ConsoleApp1Form
             BackgroundColor = ConsoleColor.Black;
             WriteLine("");
         }
-
-        static void MyFooter(string text = "Для выхода нажмите любую кнопку ...")
+        /// <summary>
+        /// Вывод моей шапки
+        /// </summary>
+        /// <param name="text"></param>
+        private static void MyFooter(string text = "Для выхода нажмите любую кнопку ...")
         {
-            WriteLine(text);
+            WriteLine("\n" + text);
             ReadKey();
         }
     }
