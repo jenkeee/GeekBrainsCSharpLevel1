bf14e1a492cb16cd1e0128947cd1054b448e6057
kanadeiar <kanadeiar@gmail.com>
2019-11-26 22:11:53 +0300

    init

---
+++ HomeWorkLesson1/ConsoleApp2BodyMassIndex/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp2BodyMassIndex
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 2.
+        /// Ввести вес и рост человека. Рассчитать и вывести индекс массы тела
+        /// (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+        /// Рассахатский
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            MyHeader(text: "Задача 2. Рассчитать и вывести индекс массы тела.");
+            ////////////////////////////////////////////////////////////////////
+
+
+
+            ////////////////////////////////////////////////////////////////////
+            MyFooter();
+        }
+
+
+        /// <summary>
+        /// Вывод моей шапки консольного приложения
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="text"></param>
+        private static void MyHeader(string title = "Geekbrains. C# Уровень 1.", string text = "")
+        {
+            Title = title;
+            WindowWidth = 120;
+            BackgroundColor = ConsoleColor.DarkGreen;
+            ForegroundColor = ConsoleColor.White;
+            WriteLine($"┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐");
+            WriteLine($"│{text,-117}│");
+            WriteLine($"└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘");
+            ForegroundColor = ConsoleColor.White;
+            BackgroundColor = ConsoleColor.Black;
+            WriteLine("");
+        }
+        /// <summary>
+        /// Вывод моей шапки
+        /// </summary>
+        /// <param name="text"></param>
+        private static void MyFooter(string text = "Для выхода нажмите любую кнопку ...")
+        {
+            WriteLine("\n" + text);
+            ReadKey();
+        }
+    }
+}
