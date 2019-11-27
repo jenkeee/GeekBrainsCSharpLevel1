da80242b35d3f1052eef7bc8bf9e2463a1ce4f5d
kanadeiar <kanadeiar@gmail.com>
2019-11-27 21:12:53 +0300

    init task 6

---
+++ HomeWorkLesson1/ConsoleApp6CreateClass/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6CreateClass
+{
+    /// <summary>
+    /// Мой класс, который может пригодиться в моей учебе
+    /// </summary>
+    class MyClass
+    {
+
+
+    }
+}
---
+++ HomeWorkLesson1/ConsoleApp6CreateClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp6CreateClass
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 6.
+        /// *Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+        /// Рассахатский
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            MyHeader(text: "Задача 6. Создать класс с методами, которые могут пригодиться в вашей учебе.");
+            ///////////////////////////////////////////////////////////////
+
+
+            ///////////////////////////////////////////////////////////////
+            MyFooter();
+        }
+
+        /// <summary>
+        /// Вывод моей шапки консольного приложения
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="text"></param>
+        private static void MyHeader(string title = "Geekbrains. C# Уровень 1.", string text = "")
+        {
+            Title = title;
+            WindowWidth = 120;
+            BackgroundColor = ConsoleColor.DarkGreen;
+            ForegroundColor = ConsoleColor.White;
+            WriteLine($"┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐");
+            WriteLine($"│{text,-117}│");
+            WriteLine($"└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘");
+            ForegroundColor = ConsoleColor.White;
+            BackgroundColor = ConsoleColor.Black;
+            WriteLine("");
+        }
+        /// <summary>
+        /// Вывод моей шапки
+        /// </summary>
+        /// <param name="text"></param>
+        private static void MyFooter(string text = "Для выхода нажмите любую кнопку ...")
+        {
+            WriteLine("\n" + text);
+            ReadKey();
+        }
+    }
+}
