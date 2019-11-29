88460411895cc2d08ca64c1af2dbb1fa0984f6f6
kanadeiar <kanadeiar@gmail.com>
2019-11-30 00:05:16 +0300

    complete task 4

--- HomeWorkLesson2/ConsoleApp4LoginPassword/Program.cs
+++ HomeWorkLesson2/ConsoleApp4LoginPassword/Program.cs
@@ -23,10 +23,54 @@ namespace ConsoleApp4LoginPassword
         {
             MyHelper.MyHeader(text:"Задача 4. Реализовать метод проверки логина и пароля.");
             ///////////////////////////////////////////////////////////////////////////////////
-
-
+            bool successValidation = SuccessValidation(); //авторизация пользователя
+            if (successValidation)
+            {
+                WriteLine("Вы успешно прошли авторизацию!");
+            }
+            else
+            {
+                WriteLine("Вы не прошли авторизацию, у вас закончились попытки!");
+            }
             ///////////////////////////////////////////////////////////////////////////////////
             MyHelper.MyFooter();
+        }
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <returns>Успешность авторизации</returns>
+        private static bool SuccessValidation()
+        {
+            int tryCount = 3; //пользователю дается три попытки
+            do
+            {
+                Write("Введите логин:>");
+                string login = ReadLine();
+                Write("Введите пароль:>");
+                string password = ReadLine();
+                if (ValidationUser(login, password)) //проверка логин - root пароль - GeegBrains
+                {
+                    break;
+                }
+                tryCount--;
+                WriteLine($"Логин/пароль неправильные, вы не прошли авторизацию, осталось попыток: {tryCount}");
+                Beep(500, 500);
+            } while (tryCount > 0);
+
+            bool successValidation = tryCount > 0;
+            return successValidation;
+        }
+        /// <summary>
+        /// Проверка логина пароля на соответствие трубуемому
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <param name="needLogin">требуемый логин</param>
+        /// <param name="needPassword">требуемый пароль</param>
+        /// <returns></returns>
+        static bool ValidationUser(string login, string password, string needLogin = "root", string needPassword = "GeekBrains")
+        {
+            return (login == needLogin && password == needPassword);
         }
     }
 }
