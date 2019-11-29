09690a2ca64e7abd7e4571524a103c0449a6df07
kanadeiar <kanadeiar@gmail.com>
2019-11-29 22:53:46 +0300

    init task 2

---
+++ HomeWorkLesson2/ConsoleApp2CountNumbersOfNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2CountNumbersOfNumber
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 2
+        /// Написать метод подсчета количества цифр числа.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text:"Задача 2. Написать метод подсчета количества цифр числа.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
