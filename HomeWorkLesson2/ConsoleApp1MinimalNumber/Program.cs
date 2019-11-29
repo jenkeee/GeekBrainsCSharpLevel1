9546e645e132ec52c57a07b8a1b5b3a0541e504d
kanadeiar <kanadeiar@gmail.com>
2019-11-29 22:13:41 +0300

    init task 1

---
+++ HomeWorkLesson2/ConsoleApp1MinimalNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1MinimalNumber
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 1.
+        /// Написать метод, возвращающий минимальное из трёх чисел.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text:"Задача 1. Написать метод, возвращающий минимальное из трёх чисел.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
