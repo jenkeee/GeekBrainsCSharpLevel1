2170a53b60624d165326603b4e8aa7e8b801a40b
kanadeiar <kanadeiar@gmail.com>
2019-11-29 23:08:52 +0300

    init task 3

---
+++ HomeWorkLesson2/ConsoleApp3CountWhenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3CountWhenNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 3
+        /// С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех
+        /// нечетных положительных чисел.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text:"Задача 3. Подсчитать сумму всех нечетных положительных чисел.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+
+        }
+    }
+}
