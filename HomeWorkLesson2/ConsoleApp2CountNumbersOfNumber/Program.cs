45f12b96d976a0b7bf328f68fbf987689bbb2393
kanadeiar <kanadeiar@gmail.com>
2019-11-29 23:03:12 +0300

    final task 2

--- HomeWorkLesson2/ConsoleApp2CountNumbersOfNumber/Program.cs
+++ HomeWorkLesson2/ConsoleApp2CountNumbersOfNumber/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp2CountNumbersOfNumber
 {
@@ -17,10 +18,26 @@ namespace ConsoleApp2CountNumbersOfNumber
         {
             MyHelper.MyHeader(text:"Задача 2. Написать метод подсчета количества цифр числа.");
             ///////////////////////////////////////////////////////////////////////////////////
-
-            
+            int number = MyHelper.GetNumberFromConsole("Введите число (int)");
+            int count = GetCountNumbersInNumber(number);
+            WriteLine($"Количество цифр в этом числе: {count}");
             ///////////////////////////////////////////////////////////////////////////////////
             MyHelper.MyFooter();
+        }
+        /// <summary>
+        /// Метод подсчета количества цифр числа
+        /// </summary>
+        /// <param name="number">число</param>
+        /// <returns>количество цифр в этом числе</returns>
+        private static int GetCountNumbersInNumber(int number)
+        {
+            int count = default;
+            while (number > 0)
+            {
+                count++;
+                number = number / 10;
+            }
+            return count;
         }
     }
 }
