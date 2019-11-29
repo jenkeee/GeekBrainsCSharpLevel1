cfbf4dabe98dda8106fd5587109b80cd4b02a1c2
kanadeiar <kanadeiar@gmail.com>
2019-11-29 23:29:09 +0300

    final task 3

--- HomeWorkLesson2/ConsoleApp3CountWhenNumbers/Program.cs
+++ HomeWorkLesson2/ConsoleApp3CountWhenNumbers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp3CountWhenNumbers
 {
@@ -18,10 +19,41 @@ namespace ConsoleApp3CountWhenNumbers
         {
             MyHelper.MyHeader(text:"Задача 3. Подсчитать сумму всех нечетных положительных чисел.");
             ///////////////////////////////////////////////////////////////////////////////////
-
+            WriteLine("Ввести 0 для выхода из цикла и показа результата.");
+            int sumNumbers = CountFromConsoleNumbers();
+            WriteLine($"Сумма нечетных и положительных чисел = {sumNumbers}");
             ///////////////////////////////////////////////////////////////////////////////////
             MyHelper.MyFooter();
-
         }
+        /// <summary>
+        /// Подсчет суммы введенных чисел с консоли - при этом нечетных и положительных
+        /// </summary>
+        /// <returns></returns>
+        private static int CountFromConsoleNumbers()
+        {
+            int sumNumbers = 0; //сумма вводимых чисел
+            while (true)
+            {
+                Write("Введите число (int):>");
+                if (Int32.TryParse(ReadLine(), out int number))
+                {
+                    if (number > 0  && number % 2 != 0)
+                    {
+                        sumNumbers += number;
+                    }
+                    else if (number == 0)
+                    {
+                        return sumNumbers;
+                    }
+                }
+                else
+                {
+                    WriteLine("Ошибка! Введен неверный формат числа! Попробуйте еще раз!");
+                    Beep(500,500);
+                }
+            }
+        }
+
+
     }
 }
