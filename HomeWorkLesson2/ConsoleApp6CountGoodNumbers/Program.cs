c6bdc7e9c937b113b9c6b3f3bae56c3b15f36d82
kanadeiar <kanadeiar@gmail.com>
2019-11-30 18:59:37 +0300

    init task 6

---
+++ HomeWorkLesson2/ConsoleApp6CountGoodNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp6CountGoodNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 6.
+        /// *Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000.
+        /// «Хорошим» называется число, которое делится на сумму своих цифр. Реализовать подсчёт
+        /// времени выполнения программы, используя структуру DateTime.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text: "Задача 6. Программа подсчета количества хороших чисел в диапазоне от 1 до 1_000_000_000.");
+            ///////////////////////////////////////////////////////////////////////////////////
+            DateTime begin = DateTime.Now;
+
+
+
+
+            WriteLine($"Время выполения программы: {DateTime.Now - begin}");
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
