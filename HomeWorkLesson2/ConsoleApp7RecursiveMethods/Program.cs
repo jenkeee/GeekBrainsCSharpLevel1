3621b1f2779fc9163f6aaadba22edb414f5e3e64
kanadeiar <kanadeiar@gmail.com>
2019-11-30 20:54:49 +0300

    final task 7

--- HomeWorkLesson2/ConsoleApp7RecursiveMethods/Program.cs
+++ HomeWorkLesson2/ConsoleApp7RecursiveMethods/Program.cs
@@ -20,14 +20,67 @@ namespace ConsoleApp7RecursiveMethods
             MyHelper.MyHeader(text: "Задача 7. Рекурсивные методы.");
             ///////////////////////////////////////////////////////////////////////////////////
             WriteLine("Пункт А. Рекурсивный метод вывода на экран чисел от a до b (a < b).");
-
-
+            var (noCancel, numberA, numberB) = InputNumbersFromUser();
+            if (noCancel)
+            {
+                PrintNumberRecursive(numberA, numberB); //рекурсивный вывод чисел на экран
+            }
+            WriteLine("\n");
             ///////////////////////////////////////////////////////////////////////////////////
-            WriteLine("Пункт Б. Рекурсивный метод суммы чисел от a до b.");
-
-            
+            WriteLine("Пункт Б. Рекурсивный метод суммы чисел от a до b. (число а может быть больше b)");
+            (noCancel, numberA, numberB) = InputNumbersFromUser();
+            if (noCancel)
+            {
+                int sum = SummNumberRecursive(numberA, numberB);
+                WriteLine($"Сумма чисел = {sum}");
+            }
             ///////////////////////////////////////////////////////////////////////////////////
             MyHelper.MyFooter();
+        }
+        /// <summary>
+        /// Ввод входных значений с консоли
+        /// </summary>
+        /// <returns>Введенные пользователем значения и признак что числа введены</returns>
+        private static (bool, int, int) InputNumbersFromUser()
+        {
+            bool noCancel = MyHelper.GetNumberFromConsole(out int numberA, "Пожалуйств введите число a (int) q-отмена");
+            if (noCancel)
+            {
+                noCancel = MyHelper.GetNumberFromConsole(out int numberB, "Пожалуйста введите число b (int) q-отмена");
+                return (noCancel, numberA, numberB);
+            }
+            return (noCancel, default, default);
+        }
+        /// <summary>
+        /// Рекурсивный вывод чисел на экран
+        /// </summary>
+        /// <param name="a">от</param>
+        /// <param name="b">до</param>
+        private static void PrintNumberRecursive(int a, int b)
+        {
+            Write($"{a} ");
+            if (a < b)
+            {
+                PrintNumberRecursive(a + 1, b);
+            }
+        }
+        /// <summary>
+        /// Рекурсивная сумма чисел
+        /// </summary>
+        /// <param name="a">от</param>
+        /// <param name="b">до</param>
+        /// <returns></returns>
+        private static int SummNumberRecursive(int a, int b)
+        {
+            if (a < b)
+            {
+                return a + SummNumberRecursive(a + 1, b);
+            }
+            if (a > b)
+            {
+                return a + SummNumberRecursive(a - 1, b);
+            }
+            return a;
         }
     }
 }
