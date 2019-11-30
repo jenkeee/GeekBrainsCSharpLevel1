1eb8975fb862de350713a4a71a57f40d097fde98
kanadeiar <kanadeiar@gmail.com>
2019-11-30 18:50:48 +0300

    final task 5

--- HomeWorkLesson2/ConsoleApp5BodyWeightIndex/MyHelper.cs
+++ HomeWorkLesson2/ConsoleApp5BodyWeightIndex/MyHelper.cs
@@ -9,21 +9,73 @@ namespace ConsoleApp5BodyWeightIndex
     static class MyHelper
     {
         /// <summary>
-        /// Ввод целого числа с консольного приложения
+        /// Ввод целого числа с консоли
         /// </summary>
-        /// <param name="message">Сообщение-приглашение для пользователя</param>
-        /// <returns>Введенное пользователем число</returns>
-        internal static int GetNumberFromConsole(string message = "Пожалуйста введите целое число (int)")
+        /// <param name="number">вертаемое введенное целое число</param>
+        /// <param name="message">сообщение приглашение</param>
+        /// <param name="min">минимум диапазона</param>
+        /// <param name="max">максимум диапазона</param>
+        /// <returns>признак что число введено</returns>
+        internal static bool GetNumberFromConsole(out int number, string message = "Пожалуйста введите целое число (int) (q-отмена)", int min = int.MinValue, int max = int.MaxValue)
         {
             while (true)
             {
                 Write($"{message}:>");
-                if (Int32.TryParse(ReadLine(), out int number))
+                string buffString = ReadLine();
+                if (Int32.TryParse(buffString, out int num)) //введено должно быть число
                 {
-                    return number;
+                    if (num < max && num > min) //число должно быть в допустимом диапазоне
+                    {
+                        number = num;
+                        return true;
+                    }
+                    WriteLine($"Ошибка! Введенное число превышает разрешенный диапазон от {min} до {max}!");
                 }
-                WriteLine("Ошибка! Введен неверный формат целого числа!");
-                Beep(500,500);
+                else if (buffString == "q") //введена пользовательская команда отмена ввода
+                {
+                    number = default;
+                    return false;
+                }
+                else //введено не число
+                {
+                    WriteLine("Ошибка! Введен неверный формат целого числа!");
+                }
+                Beep(500, 500);
+            }
+        }
+        /// <summary>
+        /// Ввод вещественного числа с консоли
+        /// </summary>
+        /// <param name="number">вертаемое введенное вещественное число</param>
+        /// <param name="message">сообщение приглашение</param>
+        /// <param name="min">минимум диапазона</param>
+        /// <param name="max">максимум диапазона</param>
+        /// <returns>признак что число введено</returns>
+        internal static bool GetNumberFromConsole(out double number, string message = "Пожалуйста введите вещественное число (double) (q-отмена)", double min = double.MinValue, double max = double.MaxValue)
+        {
+            while (true)
+            {
+                Write($"{message}:>");
+                string buffString = ReadLine();
+                if (Double.TryParse(buffString, out double num))
+                {
+                    if (num < max && num > min) //число должно быть в допустимом диапазоне
+                    {
+                        number = num;
+                        return true;
+                    }
+                    WriteLine($"Ошибка! Введенное число превышает разрешенный диапазон от {min:f2} до {max:f2}!");
+                }
+                else if (buffString == "q") //пользовательская команда отмена ввода
+                {
+                    number = default;
+                    return false;
+                }
+                else
+                {
+                    WriteLine("Ошибка! Введен неверный формат вещественного числа!");
+                }
+                Beep(500, 500);
             }
         }
 
--- HomeWorkLesson2/ConsoleApp5BodyWeightIndex/Program.cs
+++ HomeWorkLesson2/ConsoleApp5BodyWeightIndex/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleApp5BodyWeightIndex
 {
@@ -17,14 +18,91 @@ namespace ConsoleApp5BodyWeightIndex
         /// </summary>
         static void Main()
         {
-            MyHelper.MyHeader(text:"Задача 5. Программа индекс массы тела с рекомендациями по приведению ИМТ к норме.");
+            MyHelper.MyHeader(text: "Задача 5. Программа индекс массы тела с рекомендациями по приведению ИМТ к норме.");
             ///////////////////////////////////////////////////////////////////////////////////
-
-
-
-
+            WriteLine("Пункт А. Вычисление индекса массы тела и собщение, что нужно сделать чтоб была норма");
+            var (noCancel, weight, growth) = InputNumbersFromUser();
+            if (noCancel)
+            {
+                double bodyWeightIndex = CalcBodyWeightIndex(weight, growth); //вычисление индекса массы тела
+                WriteLine($"Вычисленный индекс массы тела равен {bodyWeightIndex:F1}");
+                WriteLine("Рекомендации по нормализации массы тела:");
+                WriteLine(GetRecomendationOfIndex(bodyWeightIndex)); //получение рекомендации на основе индекса
+                WriteLine("Для продолжения нажмите любую кнопку ...");
+                ReadKey();
+            }
+            ///////////////////////////////////////////////////////////////////////////////////
+            WriteLine("\nПункт Б. Расчет, на сколько килограмм пополнеть или похудеть чтоб привести туловище в норму");
+            (noCancel, weight, growth) = InputNumbersFromUser();
+            if (noCancel)
+            {
+                double bodyWeightIndex = CalcBodyWeightIndex(weight, growth); //вычисление индекса массы тела
+                WriteLine($"Вычисленный индекс массы тела равен {bodyWeightIndex:F1}");
+                WriteLine("Рекомандации насчет корректировки массы тела:");
+                WriteLine(GetCountRecomendationOfIndex(bodyWeightIndex,growth, weight));
+            }
             ///////////////////////////////////////////////////////////////////////////////////
             MyHelper.MyFooter();
+        }
+        /// <summary>
+        /// Расчет индекса массы тела
+        /// </summary>
+        /// <param name="weight">вес</param>
+        /// <param name="growth">рост</param>
+        /// <returns>индекс массы тела</returns>
+        private static double CalcBodyWeightIndex(double weight, double growth)
+        {
+            return weight / (growth * growth / 10000);
+        }
+        /// <summary>
+        /// Ввод входных значений с консоли
+        /// </summary>
+        /// <returns>Введенные пользователем значения и признак что числа введены</returns>
+        private static (bool, int, double) InputNumbersFromUser()
+        {
+            bool noCancel = MyHelper.GetNumberFromConsole(out int weight, "Пожалуйств введите массу тела в килограммах (int) q-отмена", 10, 300);
+            if (noCancel)
+            {
+                noCancel = MyHelper.GetNumberFromConsole(out double growth, "Пожалуйста введите рост человека в сантиметрах (double) q-отмена", 50.0, 200.0);
+                return (noCancel, weight, growth);
+            }
+            return (noCancel, default, default);
+        }
+        /// <summary>
+        /// Получение рекомендации по корректировке веса тела
+        /// </summary>
+        /// <param name="BWI">индекс массы тела</param>
+        /// <returns>Рекомендация</returns>
+        private static string GetRecomendationOfIndex(double BWI)
+        {
+            return BWI switch
+            {
+                double d when d < 16.0 => "Выраженный дефицит массы тела. Немедленно набрать очень много веса!",
+                double d when d < 18.9 => "Недостаточная (дефицит) масса тела. Очень срочно набрать вес!",
+                double d when d < 24.99 => "Норма. Срочно набрать вес!",
+                double d when d < 30.0 => "Избыточная масса тела (предожирение). Немного набрать вес.",
+                double d when d < 35.0 => "Ожирение (на самом деле это нормальный вес!). Все в порядке!",
+                double d when d < 40.0 => "Ожирение резкое. Немножечко сбросить вес!",
+                _ => "Очень резкое ожирение. Срочно сбросить вес!",
+            };
+        }
+        /// <summary>
+        /// Получение рекомендации по корректировке веса тела в числах
+        /// </summary>
+        /// <param name="BWI"></param>
+        /// <param name="growth"></param>
+        /// <param name="weight"></param>
+        /// <returns></returns>
+        private static string GetCountRecomendationOfIndex(double BWI, double growth, double weight)
+        {
+            double standardWeightMin = 30 * (growth * growth / 10000); //стандартный вес по росту минимум
+            double standardWeightMax = 35 * (growth * growth / 10000); //стандартный вес по росту максимум
+            return BWI switch
+            {
+                double d when d < 30 => $"Человеку для того чтобы имено нормальный ИМТ (Легкое ожирение) нужно поправится на {standardWeightMin - weight} килограмм.",
+                double d when d > 35 => $"Человеку для того чтобы имено нормальный ИМТ (Легкое ожирение) нужно похудеть на {weight - standardWeightMax} килограмм.",
+                _ => "Нормальный вес, корректировка не требуется.",
+            };
         }
     }
 }
