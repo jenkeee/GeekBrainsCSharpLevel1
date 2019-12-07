718eed03f450299f1f0fa8b5c8f1a11660fbc36a
kanadeiar <kanadeiar@gmail.com>
2019-12-08 01:28:05 +0300

    init task 5

---
+++ HomeWorkLesson4/ClassLibrary2DoubleArray/DoubleArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2DoubleArray
+{
+    /// <summary>
+    /// Класс работы с двумерным массивом
+    /// </summary>
+    public class DoubleArray
+    {
+        private int[,] doubleArr;
+        /// <summary>
+        /// Конструктор с заполнением массива случайными числами
+        /// </summary>
+        /// <param name="sizeX">число колонок</param>
+        /// <param name="sizeY">число строк</param>
+        public DoubleArray(int sizeX, int sizeY)
+        {
+            Random rnd = new Random();
+            doubleArr = new int[sizeX, sizeY];
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    doubleArr[i, j] = rnd.Next(1, 1001);
+                }
+            }
+        }
+
+
+    }
+}
