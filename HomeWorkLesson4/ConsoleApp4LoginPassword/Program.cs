766b203b6cb651408ec5ae0308c625ddaf8291f6
kanadeiar <kanadeiar@gmail.com>
2019-12-07 23:51:36 +0300

    init task 4

---
+++ HomeWorkLesson4/ConsoleApp4LoginPassword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4LoginPassword
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 4
+        /// Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив.
+        /// Создайте структуру Account, содержащую Login и Password.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text: "Задача 4. Логины и пароли из файла. ");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
