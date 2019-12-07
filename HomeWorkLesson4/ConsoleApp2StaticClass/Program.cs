605885512bbf58a99cb4bff3a2891f5f22138e23
kanadeiar <kanadeiar@gmail.com>
2019-12-07 20:27:22 +0300

    init task 2 b

--- HomeWorkLesson4/ConsoleApp2StaticClass/Program.cs
+++ HomeWorkLesson4/ConsoleApp2StaticClass/Program.cs
@@ -18,6 +18,18 @@ namespace ConsoleApp2StaticClass
         {
             MyHelper.MyHeader(text: "Задача 2. Добавление статического класса StaticClass.");
             ///////////////////////////////////////////////////////////////////////////////////
+            WriteLine("Пункт А. Статический класс решения поставленной задачи.");
+            int[] arrInts = StaticClass.GetArrayWithRandomNum(20); //получить массив
+            WriteLine("Массив элементов из случайных чисел:");
+            Array.ForEach(arrInts, WriteLine); //вывод массива
+            WriteLine();
+            int count = StaticClass.GetCountGoodNumbers(arrInts); //получить количество пар
+            WriteLine($"Кол-во пар элементов массива, в которых только одно число делится на три = {count}");
+            MyHelper.MyPause();
+            ///////////////////////////////////////////////////////////////////////////////////
+            WriteLine("Пункт Б. Статический метод считывания массива из текстового файла.");
+            
+            StaticClass.SaveArrayToFile(arrInts,"data.dat");
 
 
             ///////////////////////////////////////////////////////////////////////////////////
