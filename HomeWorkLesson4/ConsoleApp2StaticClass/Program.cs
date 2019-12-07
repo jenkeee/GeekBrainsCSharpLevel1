2eb37781b1b83362d88046039edd03ae8ae4e579
kanadeiar <kanadeiar@gmail.com>
2019-12-07 20:00:09 +0300

    init task 2

---
+++ HomeWorkLesson4/ConsoleApp2StaticClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp2StaticClass
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 2
+        ///
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text: "Задача 2. Добавление статического класса StaticClass.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
