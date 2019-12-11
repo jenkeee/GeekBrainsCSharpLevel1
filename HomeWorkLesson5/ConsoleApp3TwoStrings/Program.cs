3da418a7093637a93412be3f7ca215997fc3a91f
kanadeiar <kanadeiar@gmail.com>
2019-12-11 06:16:12 +0300

    init task 3

---
+++ HomeWorkLesson5/ConsoleApp3TwoStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3TwoStrings
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 3.
+        /// *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+        /// Например:
+        /// badc являются перестановкой abcd.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text: "Задача 3. Определение что две строки являются перестановками или нет.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
