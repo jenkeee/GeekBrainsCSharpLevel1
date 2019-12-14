10b6cff6c8f4bac4d2c2f762403404409250a8fc
kanadeiar <kanadeiar@gmail.com>
2019-12-14 22:38:20 +0300

    init task 4

---
+++ HomeWorkLesson6/ConsoleApp4ReadFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4ReadFile
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 4.
+        /// **Считайте файл различными способами. Смотрите “Пример записи файла различными способами”.
+        /// Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), строку для
+        /// StreamReader и массив int для BinaryReader.
+        /// Рассахатский
+        /// </summary>
+        static void Main()
+        {
+            MyHelper.MyHeader(text: "Задача 4. Чтение файла различными способами.");
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            MyHelper.MyFooter();
+        }
+    }
+}
