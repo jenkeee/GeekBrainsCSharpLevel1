9ea0451dc1e53442a74dace61043031076fb1a4d
kanadeiar <kanadeiar@gmail.com>
2019-12-18 00:05:09 +0300

    final task 1

--- HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.Designer.cs
+++ HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.Designer.cs
@@ -30,6 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelPlayerNumber = new System.Windows.Forms.Label();
+            this.labelCommandCount = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMulTwo = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonUndo = new System.Windows.Forms.Button();
+            this.labelWinner = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.начатьНовуюИгруToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -37,29 +51,178 @@
             this.label1.BackColor = System.Drawing.Color.LightGreen;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(0, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(604, 55);
+            this.label1.Size = new System.Drawing.Size(480, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "Задача 1. Программа удвоитель.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Текущее число:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Количество команд:";
+            // 
+            // labelPlayerNumber
+            // 
+            this.labelPlayerNumber.BackColor = System.Drawing.Color.Lime;
+            this.labelPlayerNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPlayerNumber.Location = new System.Drawing.Point(29, 120);
+            this.labelPlayerNumber.Name = "labelPlayerNumber";
+            this.labelPlayerNumber.Size = new System.Drawing.Size(150, 33);
+            this.labelPlayerNumber.TabIndex = 3;
+            this.labelPlayerNumber.Text = "0";
+            this.labelPlayerNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCommandCount
+            // 
+            this.labelCommandCount.BackColor = System.Drawing.Color.Cyan;
+            this.labelCommandCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCommandCount.Location = new System.Drawing.Point(29, 189);
+            this.labelCommandCount.Name = "labelCommandCount";
+            this.labelCommandCount.Size = new System.Drawing.Size(150, 33);
+            this.labelCommandCount.TabIndex = 4;
+            this.labelCommandCount.Text = "0";
+            this.labelCommandCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1Doubler.Properties.Resources.КУр2;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 254);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 162);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(281, 95);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(157, 70);
+            this.buttonPlus.TabIndex = 6;
+            this.buttonPlus.Text = "+1";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMulTwo
+            // 
+            this.buttonMulTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMulTwo.Location = new System.Drawing.Point(281, 178);
+            this.buttonMulTwo.Name = "buttonMulTwo";
+            this.buttonMulTwo.Size = new System.Drawing.Size(157, 70);
+            this.buttonMulTwo.TabIndex = 7;
+            this.buttonMulTwo.Text = "x2";
+            this.buttonMulTwo.UseVisualStyleBackColor = true;
+            this.buttonMulTwo.Click += new System.EventHandler(this.buttonMulTwo_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(281, 261);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(157, 70);
+            this.buttonReset.TabIndex = 8;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonUndo
+            // 
+            this.buttonUndo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUndo.Location = new System.Drawing.Point(281, 344);
+            this.buttonUndo.Name = "buttonUndo";
+            this.buttonUndo.Size = new System.Drawing.Size(157, 70);
+            this.buttonUndo.TabIndex = 9;
+            this.buttonUndo.Text = "Отмена";
+            this.buttonUndo.UseVisualStyleBackColor = true;
+            this.buttonUndo.Click += new System.EventHandler(this.buttonUndo_Click);
+            // 
+            // labelWinner
+            // 
+            this.labelWinner.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWinner.Location = new System.Drawing.Point(0, 443);
+            this.labelWinner.Name = "labelWinner";
+            this.labelWinner.Size = new System.Drawing.Size(480, 26);
+            this.labelWinner.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.начатьНовуюИгруToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(480, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // начатьНовуюИгруToolStripMenuItem
+            // 
+            this.начатьНовуюИгруToolStripMenuItem.Name = "начатьНовуюИгруToolStripMenuItem";
+            this.начатьНовуюИгруToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.начатьНовуюИгруToolStripMenuItem.Text = "Начать новую игру";
+            this.начатьНовуюИгруToolStripMenuItem.Click += new System.EventHandler(this.начатьНовуюИгруToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 422);
+            this.ClientSize = new System.Drawing.Size(480, 469);
+            this.Controls.Add(this.labelWinner);
+            this.Controls.Add(this.buttonUndo);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonMulTwo);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelCommandCount);
+            this.Controls.Add(this.labelPlayerNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Geekbrains. C# Уровень 1.";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelPlayerNumber;
+        private System.Windows.Forms.Label labelCommandCount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMulTwo;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonUndo;
+        private System.Windows.Forms.Label labelWinner;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem начатьНовуюИгруToolStripMenuItem;
     }
 }
 
--- HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.cs
+++ HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace WindowsFormsApp1Doubler
 {
     public partial class Form1 : Form
     {
+        private Doubler doubler;
         /// <summary>
         /// Задача 1.
         /// а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
@@ -25,6 +27,47 @@ namespace WindowsFormsApp1Doubler
         public Form1()
         {
             InitializeComponent();
+            doubler = new Doubler();
+        }
+		//перерисовка
+        private void Repaint()
+        {
+            var (number, command, winner) = doubler.GetAllForForm();
+            labelPlayerNumber.Text = number;
+            labelCommandCount.Text = command;
+            labelWinner.Text = winner;
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            bool win = doubler.Plus();
+            Repaint();
+            if (win) MessageBox.Show("Вы победили в этой игре!", "Поздравления", MessageBoxButtons.OK);
+        }
+
+        private void buttonMulTwo_Click(object sender, EventArgs e)
+        {
+            bool win = doubler.MultiplyTwo();
+            Repaint();
+            if (win) MessageBox.Show("Вы победили в этой игре!", "Поздравления", MessageBoxButtons.OK);
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            doubler.Reset();
+            Repaint();
+        }
+
+        private void buttonUndo_Click(object sender, EventArgs e)
+        {
+            doubler.Undo();
+            Repaint();
+        }
+
+        private void начатьНовуюИгруToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            doubler.RestartGame();
+            Repaint();
         }
     }
 }
