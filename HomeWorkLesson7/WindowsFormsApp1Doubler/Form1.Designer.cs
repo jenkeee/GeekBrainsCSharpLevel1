580f75cc07ce3f674a9520b72e61a87bf3ca0d07
kanadeiar <kanadeiar@gmail.com>
2019-12-17 22:46:08 +0300

    init task 1

---
+++ HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp1Doubler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGreen;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(604, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Задача 1. Программа удвоитель.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 422);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Geekbrains. C# Уровень 1.";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HomeWorkLesson7/WindowsFormsApp1Doubler/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1Doubler
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Задача 1.
+        /// а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+        /// б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число
+        /// должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+        /// в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте
+        /// обобщенный класс Stack.
+        /// Вся логика игры должна быть реализована в классе с удвоителем.
+        /// Рассахатский
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
