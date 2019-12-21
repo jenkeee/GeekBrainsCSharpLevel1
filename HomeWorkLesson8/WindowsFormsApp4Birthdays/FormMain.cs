6e220ec9a83564591d61820f303141bfacac015d
kanadeiar <kanadeiar@gmail.com>
2019-12-21 17:42:32 +0300

    init task 4

---
+++ HomeWorkLesson8/WindowsFormsApp4Birthdays/FormMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4Birthdays
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 4
+        /// *Используя полученные знания и класс TrueFalse в качестве шаблона, разработать
+        /// собственную утилиту хранения данных (Например: Дни рождения, Траты, Напоминалка,
+        /// Английские слова и другие).
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 8. C# Уровень 1. Программирование “по-взрослому.\n" +
+                               "Задача 4. Собственная утилита хранения Дней рождения.";
+        }
+    }
+}
