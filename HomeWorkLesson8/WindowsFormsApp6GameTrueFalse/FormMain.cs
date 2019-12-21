e6d7a0805dac1840549763feda4b0459782fd184
kanadeiar <kanadeiar@gmail.com>
2019-12-22 02:36:20 +0300

    init task 6

---
+++ HomeWorkLesson8/WindowsFormsApp6GameTrueFalse/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6GameTrueFalse
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 6
+        /// Игра "Верю - не верю"
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 8. C# Уровень 1. Программирование \"по-взрослому\"\n" +
+                               "Задача 6. Игра \"Верю - не верю\".";
+        }
+    }
+}
