3aac0d3be6a2002c69af6d46cabc62b5dd7e2592
kanadeiar <kanadeiar@gmail.com>
2019-12-21 08:43:06 +0300

    init task 3

---
+++ HomeWorkLesson8/WindowsFormsApp3GameEditor/FormMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp3GameEditor
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.BackColor = System.Drawing.Color.LightGreen;
+            this.labelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeader.Location = new System.Drawing.Point(0, 0);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(643, 74);
+            this.labelHeader.TabIndex = 3;
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 397);
+            this.Controls.Add(this.labelHeader);
+            this.Name = "FormMain";
+            this.Text = "Geekbrains. C# Уровень 1.";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+    }
+}
+
---
+++ HomeWorkLesson8/WindowsFormsApp3GameEditor/FormMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3GameEditor
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 3.
+        /// а) Создать приложение, показанное на уроке, добавив в него защиту от возможных ошибок (не создана база
+        /// данных, обращение к несуществующему вопросу, открытие слишком большого файла и т.д.).
+        /// б) Изменить интерфейс программы, увеличив шрифт, поменяв цвет элементов и добавив другие «косметические»
+        /// улучшения на свое усмотрение.
+        /// в) Добавить в приложение меню «О программе» с информацией о программе (автор, версия, авторские права и др.).
+        /// г)* Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных (элемент SaveFileDialog).
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 8. C# Уровень 1. Программирование “по-взрослому.\n" +
+                               "Задача 3. Приложение редактор вопросов игры верю - не верю.";
+        }
+    }
+}
