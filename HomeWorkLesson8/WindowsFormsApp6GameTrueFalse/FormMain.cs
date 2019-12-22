22ebdf053a987492791ff1371fb3ec985da59bf0
kanadeiar <kanadeiar@gmail.com>
2019-12-22 11:26:13 +0300

    complete class game

--- HomeWorkLesson8/WindowsFormsApp6GameTrueFalse/FormMain.cs
+++ HomeWorkLesson8/WindowsFormsApp6GameTrueFalse/FormMain.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApp6GameTrueFalse
 {
     public partial class FormMain : Form
     {
+        private GameManager manager;
         /// <summary>
         /// Задача 6
         /// Игра "Верю - не верю"
@@ -22,6 +23,30 @@ namespace WindowsFormsApp6GameTrueFalse
             InitializeComponent();
             labelHeader.Text = "Методичка 8. C# Уровень 1. Программирование \"по-взрослому\"\n" +
                                "Задача 6. Игра \"Верю - не верю\".";
+            manager = new GameManager();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            manager.SetForm(labelText, buttonGo, buttonTrue, buttonFalse);
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            manager.GoGame();
+            manager.SetForm(labelText, buttonGo, buttonTrue, buttonFalse);
+        }
+
+        private void buttonTrue_Click(object sender, EventArgs e)
+        {
+            manager.Answer(true);
+            manager.SetForm(labelText, buttonGo, buttonTrue, buttonFalse);
+        }
+
+        private void buttonFalse_Click(object sender, EventArgs e)
+        {
+            manager.Answer(false);
+            manager.SetForm(labelText, buttonGo, buttonTrue, buttonFalse);
         }
     }
 }
