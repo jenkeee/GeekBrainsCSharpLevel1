a73185345ed6d5bc946c9ff5585b41106531fd62
kanadeiar <kanadeiar@gmail.com>
2019-12-22 12:08:07 +0300

    init task 7

---
+++ HomeWorkLesson8/WindowsFormsApp7WeatherForecast/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7WeatherForecast
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// Задача 7
+        /// Программа прогноза погоды
+        /// Рассахатский
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            labelHeader.Text = "Методичка 8. C# Уровень 1. Программирование \"по-взрослому\"\n" +
+                               "Задача 7. Программа прогноза погоды.";
+        }
+    }
+}
